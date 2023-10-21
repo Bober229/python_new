55ed928dc51f9c93fdb7f63831f5739a5ef9dd25
c2012 <c2012@step.lv>
2023-10-21 13:56:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,41 @@
 #     i += 1
 # print(i)
 
-a = ["ananas","apple","peach","candy","pear","neracuja","lemon","sweets"]
-for i in range(1):
-    if i == "candy" or i == "sweets":
-            a.remove(i)
-    # a.remove("candy")
-    # a.remove("sweets")
-print(a)
+# a = ["ananas","apple","peach","candy","pear","neracuja","lemon","sweets"]
+# for i in range(1):
+#     if i == "candy" or i == "sweets":
+#             a.remove(i)
+#     # a.remove("candy")
+#     # a.remove("sweets")
+# print(a)
+# a = 5
+# def add():
+#     a = 3
+#     a += 5
+#     print(a)
+#
+# add()
+# print(a)
+
+# class Student:
+#     def __init__(self, name, age=12):
+#         self.name = name
+#         self.age = age
+#     def printer(self):
+#         print(self.name, self.age)
+#
+#
+# danylo = Student(name="Danylo", age = 13)
+# zlata = Student(name="Zlata", age = 15)
+# zlata.printer()
+
+class Dino:
+    def __init__(self, name, kg=1, age=1):
+        self.name = name
+        self.kg = kg
+        self.age = age
+    def printer(self):
+            print(self.name, self.kg, self.age)
+
+trex = Dino(name="T-Rex", kg = 150)
+trex.printer()

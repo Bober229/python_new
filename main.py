5327a8844974c24f6080068ec7b1c6f4d4766018
c2012 <c2012@step.lv>
2023-10-21 12:46:23 +0300

    1

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 a = int(input("Введіть число: "))
 sum = 0
-
+i = 0
 while sum <= 100:
     print(sum)
     sum += a
-    print(f"Число зміної a дорівнює {a}")
+    # print(f"Число зміної a дорівнює {a}")
+    i += 1
+print(i)

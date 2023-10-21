6553317f3e2596b807870331c528b533d8082a38
c2012 <c2012@step.lv>
2023-10-21 12:58:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-a = int(input("Введіть число: "))
-sum = 0
-i = 0
-while sum <= 100:
-    print(sum)
-    sum += a
-    # print(f"Число зміної a дорівнює {a}")
-    i += 1
-print(i)
+# a = int(input("Введіть число: "))
+# sum = 0
+# i = 0
+# while sum <= 100:
+#     print(sum)
+#     sum += a
+#     # print(f"Число зміної a дорівнює {a}")
+#     i += 1
+# print(i)
+
+list = ["ananas","apple","peach","candy","pear","neracuja","lemon","sweets"]
+for i in range(1):
+    list.remove("peach")
+print(list)

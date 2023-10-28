233855fee5b66fb7f6923d13c8683ec0e6a8c451
Bober <112754239+Bober229@users.noreply.github.com>
2023-10-28 11:12:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-kickk = ''
+menu = input("Привіт! Це головне меню, якщо вихочите подивитись список працівників напишіть '1': \nЯкщо ви хочите змінивік людини, напишіть '2': ")
 class Workers:
     def __init__(self, name, surname, old = 18, country="USA"):
         self.name = name
@@ -8,12 +8,19 @@ class Workers:
     def printer(self):
             print(self.name, self.surname, self.old, self.country)
 
-denis = Workers(name="Денис", surname ="Міллер", old = 19, country="United Kingdom")
-dmitro = Workers(name="Дмитро", surname="Геймер", old = 25, country="USA")
-dgordg = Workers(name="Джордж", surname="Королевий", old = 23, country="United Kingdom")
-maksym = Workers(name="Максим", surname="Дмитрович", old = 21, country="USA")
-old = ""
-menu = input("Привіт! Це головне меню, якщо вихочите подивитись список працівників напишіть '1': \nЯкщо ви хочите змінивік людини, напишіть '2': ")
+def main()
+    name = input("Name")
+    workerss =     denis = Workers(name="Денис", surname="Міллер", old=19, country="United Kingdom")
+    dmitro = Workers(name="Дмитро", surname="Геймер", old=25, country="USA")
+    dgordg = Workers(name="Джордж", surname="Королевий", old=23, country="United Kingdom")
+    maksym = Workers(name="Максим", surname="Дмитрович", old=21, country="USA")
+    for i range(1):
+
+if menu == '2':
+    old2 = input("Впишіть імя: ")
+    old1 = input("Впишіть на скільки років він виріс: ")
+
+
 if menu == '1':
     printero = input("Виберіть людину, якої інформації хочете подивитись: ")
     if printero == "Денис":
@@ -23,16 +30,4 @@ if menu == '1':
     elif printero == "Джордж":
         dgordg.printer()
     elif printero == "Максим":
-        maksym.printer()
-elif menu == '2':
-    old2 = input("Впишіть імя: ")
-    old1 = input("Впишіть на скільки років він виріс: ")
-    if old2 == "Денис":
-
-        denis.printer()
-    elif old2 == "Дмитро":
-        dmitro.printer()
-    elif old2 == "Джордж":
-        dgordg.printer()
-    elif old2 == "Максим":
         maksym.printer()

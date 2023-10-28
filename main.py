3958ae52377baf6971a751021de16356b012f5be
c2012 <c2012@step.lv>
2023-10-28 12:47:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-num = int(input("Введіть число:"))
-#num1 = int(input("Введіть число:"))
+num = 5
+num1 = 21
+nnum = 0
 if num <= 47:
-    
-# if num <= 47:
-#     print(num*7-47)
-
-# if num <= 47:
-#     print((47-num)/num1)
+    while nnum < num:
+        print(num+5==nnum)

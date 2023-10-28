7e5e5a8edcc48b24df7c8851c70036455b462657
Bober <112754239+Bober229@users.noreply.github.com>
2023-10-28 11:24:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 menu = input("Привіт! Це головне меню, якщо вихочите подивитись список працівників напишіть '1': \nЯкщо ви хочите змінивік людини, напишіть '2': ")
+if menu == '2':
+    old2 = input("Впишіть імя: ")
+    old1 = input("Впишіть на скільки років він виріс: ")
 class Workers:
     def __init__(self, name, surname, old = 18, country="USA"):
         self.name = name
@@ -7,20 +10,19 @@ class Workers:
         self.country = country
     def printer(self):
             print(self.name, self.surname, self.old, self.country)
-
-def main()
-    name = input("Name")
-    workerss =     denis = Workers(name="Денис", surname="Міллер", old=19, country="United Kingdom")
-    dmitro = Workers(name="Дмитро", surname="Геймер", old=25, country="USA")
-    dgordg = Workers(name="Джордж", surname="Королевий", old=23, country="United Kingdom")
-    maksym = Workers(name="Максим", surname="Дмитрович", old=21, country="USA")
-    for i range(1):
-
-if menu == '2':
-    old2 = input("Впишіть імя: ")
-    old1 = input("Впишіть на скільки років він виріс: ")
+    def printerage(self, old, name, old1):
+        if old2 == self.name:
+            old1 += self.old
+            print(old2)
+        print(old)
 
 
+
+denis = Workers(name="Денис", surname="Міллер", old=19, country="United Kingdom")
+dmitro = Workers(name="Дмитро", surname="Геймер", old=25, country="USA")
+dgordg = Workers(name="Джордж", surname="Королевий", old=23, country="United Kingdom")
+maksym = Workers(name="Максим", surname="Дмитрович", old=21, country="USA")
+denis.printerage()
 if menu == '1':
     printero = input("Виберіть людину, якої інформації хочете подивитись: ")
     if printero == "Денис":

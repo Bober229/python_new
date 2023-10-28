e188030cd251fb72573f83510c1e50c187909347
c2012 <c2012@step.lv>
2023-10-28 14:24:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,18 @@
 num = 6
 num1 = 11
 target = 333
-a = 7
-b = 5
-d = target-num1
+a = 3
+b = 2
+d = 0
 c = 0
+if num1 != a:
+    c = target-num
+    c = c/a-num1-num
+if num1 >= num:
+    d = (target/111-3) + num1
 if target != num1:
-    d = target-num1
-    d = d/a
-if num1 >= 0:
-    c = d-num
-
+    d = (d-num1)
 print(f"Мінімальна кількикість монет, до суми {target}: ")
 print(f"{a} монеток по {c}")
 print(f"{b} по {d}")
+print(b*d+a*c)

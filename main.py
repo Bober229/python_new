41223f6c8ee6ca1518e19c3e855a2ce24199524a
c2012 <c2012@step.lv>
2023-10-28 12:41:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 num = int(input("Введіть число:"))
 #num1 = int(input("Введіть число:"))
 if num <= 47:
-    print(num*7-47)
+    
+# if num <= 47:
+#     print(num*7-47)
 
 # if num <= 47:
 #     print((47-num)/num1)

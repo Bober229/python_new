d673fcea106b5a47f867c1ea00cd10fe4cbb33ed
c2012 <c2012@step.lv>
2023-11-04 14:25:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-words = ["Поле", "Урок", "Дота", "Чудо"]
+words = ["Поле", "Поле", "Дота", "Чудо"]
 import random
 #randnum = random.randint(0, 3)
 randnum = random.randint(0, 1)
 print(words[randnum])
 print(len(words))
+lens = len(words)
 startgame = input("Вітаємо, друже у грі. Поле Чудес! Якщо ти готовий, нажми 'ENTER' (P:S У вас 5 спроб!) ")
 if words == words:
     for playerword in range(5):
         if words[randnum] == "Поле":
             print("Місце де садять овочі?")
-            word = input("Впишіть букву (Або ж ціле слово!):")
+            word = input("Впишіть букву:")
             if word == words[0] or words[1] or words[2] or words[3]:
                 print("Чудово! Ви вгадали першу слово!")
                 if len(words) >= 4 and word == words[0]:
@@ -19,18 +20,9 @@ if words == words:
         if words[randnum] == "Урок":
             print("Де сидять школярі?")
             word = input("Впишіть букву:")
-            if len(word) == words[randnum]:
-                if word == words:
-                    print("Ура! Ви вгадали усе слово! Вітаємо!")
-                    print(words)
-                    break
-                else:
-                    print("О ніі!! Ви хотіли вгадати усе слово? Але у вас не вийшло! Гра закінчилась!")
-
-
-            elif word == words[0] or words[1] or words[2] or words[3]:
+            if word == words[0] or words[1] or words[2] or words[3]:
                 print("Чудово! Ви вгадали перше слово!")
-                if len(words) >= 4 and word == words[0]:
+                if lens == 4 and word == words[0]:
                     print(f"{words[0]}■■■")
             else:
                 print("Викличте адмінів, гра зламалась :o ")

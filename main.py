55b50a93c1721909102ca2c3ddcf89b07099af32
c2012 <c2012@step.lv>
2023-11-04 14:22:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,9 @@ if words == words:
     for playerword in range(5):
         if words[randnum] == "Поле":
             print("Місце де садять овочі?")
-            word = input("Впишіть букву:")
-            if len(word) == words[randnum]:
-                if word == words:
-                    print("Ура! Ви вгадали усе слово! Вітаємо!")
-                    print(words)
-                    break
-                else:
-                    print("О ніі!! Ви хотіли вгадати усе слово? Але у вас не вийшло! Гра закінчилась!")
-
-
-            elif word == words[0] or words[1] or words[2] or words[3]:
-                print("Чудово! Ви вгадали перше слово!")
+            word = input("Впишіть букву (Або ж ціле слово!):")
+            if word == words[0] or words[1] or words[2] or words[3]:
+                print("Чудово! Ви вгадали першу слово!")
                 if len(words) >= 4 and word == words[0]:
                     print(f"{words[0]}■■■")
             else:
@@ -29,7 +20,7 @@ if words == words:
             print("Де сидять школярі?")
             word = input("Впишіть букву:")
             if len(word) == words[randnum]:
-                if word == words[randnum]:
+                if word == words:
                     print("Ура! Ви вгадали усе слово! Вітаємо!")
                     print(words)
                     break

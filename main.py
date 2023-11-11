f6987a8e607f195ca85df87c83f6aa3d38d4c92e
c2012 <c2012@step.lv>
2023-11-11 12:32:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 class Human():
     age = 24
-    job = "-"
+    glass_color = "green"
+    job = ""
 
 class Worker(Human):
     age = 35
-    job = "Офісник працівник"
+    parent_glass_color = "blue"
+    job = "Лікар"
 
 class Student(Human):
-    age = 19
-    parent_job = "-"
+    parent_age = 19
+    glass_color = "brown"
+    university = "IT University School"
 
 work = Worker()
 stund = Student()
 human = Human()
 #print(human.age, human.job)
-print(work.age, work.job)
-print(stund.age, stund.job)
+print(work.age, work.job, work.parent_glass_color)
+print(stund.parent_age, stund.university, stund.glass_color)

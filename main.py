289af076322e33e7dfb0ac7f855daecfe89e37b0
c2012 <c2012@step.lv>
2023-11-11 12:45:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
-class Human():
-    age = 24
-    glass_color = "green"
-    job = ""
+# class Human():
+#     age = 24
+#     glass_color = "green"
+#     job = ""
+#
+# class Worker(Human):
+#     age = 35
+#     parent_glass_color = "blue"
+#     job = "Лікар"
+#
+# class Student(Human):
+#     parent_age = 19
+#     glass_color = "brown"
+#     university = "IT University School"
+#
+# work = Worker()
+# stund = Student()
+# human = Human()
+# #print(human.age, human.job)
+# print(work.age, work.job, work.parent_glass_color)
+# print(stund.parent_age, stund.university, stund.glass_color)
 
-class Worker(Human):
+class University():
+    name = "Epic IT"
+
+class Group(University):
     age = 35
     parent_glass_color = "blue"
     job = "Лікар"
 
-class Student(Human):
-    parent_age = 19
-    glass_color = "brown"
-    university = "IT University School"
+class Students(Group):
+    age = 19
+    name = "Денис"
+    parent_group = "4"
 
-work = Worker()
-stund = Student()
-human = Human()
+class Teacher(University):
+    tname = "Дмитро"
+    tage = 24
+    parent_group = ""
+    
+
+class Subjects(Students, Teacher):
+    subname = "Інформатика"
+    mark = "10"
+
+#a = int(input("Введіть 1 - додати вчителя. \nВведіть 2 - щоб додат студента:"))
+
+univer = University()
+group = University()
+stund = Students()
+teacher = Teacher()
+subjects = Subjects()
 #print(human.age, human.job)
-print(work.age, work.job, work.parent_glass_color)
+print(group.p)
 print(stund.parent_age, stund.university, stund.glass_color)

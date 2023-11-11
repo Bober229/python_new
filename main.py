bd83f2f32adbdc21008819fdfd81985640705764
c2012 <c2012@step.lv>
2023-11-11 13:29:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -28,34 +28,66 @@ class Group(University):
     group = "4"
 
 class Students(Group):
-    age = 19
-    name = "Денис"
-    parent_group = "4"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-class Subjects(Students, Teacher):
+    def printer(self):
+        print(self.name, self.age)
+
+
+pavel = Students(name="Павло", age="16")
+vadim = Students(name="Вадим", age="17")
+oleksander = Students(name="Олександр", age="31")
+class Teachers(University):
+    def __init__(self, tname, tage):
+        self.tname = tname
+        self.tage = tage
+
+    def printer(self):
+        print(self.tname, self.tage)
+dmitro = Teachers(tname="Дмитро", tage="24")
+pavel = Teachers(tname="Павло", tage="18")
+denis = Teachers(tname="Денис", tage="31")
+a = int(input("Введіть 1 - додати вчителя/вивести вчителів. \nВведіть 2 - додати студента/вивести студентів: "))
+if a == 1:
+    teacher = int(input("Що ви хочите? Вивести вчителів - 1, додати вчителів - 2: "))
+    if teacher == 1:
+        dmitro.printer()
+        pavel.printer()
+        denis.printer()
+    if teacher == 2:
+        newname = input("Введіть імя, нового вчителя: ")
+        newage = input("Введіть вік, нового вчителя: ")
+        newname = Teachers(tname=f"{newname}", tage=f"{newage}")
+        dmitro.printer()
+        pavel.printer()
+        denis.printer()
+        newname.printer()
+if a == 2:
+    students = int(input("Що ви хочите? Вивести вчителів - 1, додати вчителів - 2: "))
+    if students == 1:
+        dmitro.printer()
+        pavel.printer()
+        denis.printer()
+    if students == 2:
+        newname = input("Введіть імя, нового вчителя: ")
+        newage = input("Введіть вік, нового вчителя: ")
+        newname = Teachers(tname=f"{newname}", tage=f"{newage}")
+        dmitro.printer()
+        pavel.printer()
+        denis.printer()
+        newname.newteacher()
+
+class Subjects(Students, Teachers):
     subs = ["Математика", "ІТ", "Фізика", "Фізкультура"]
     marks = [12,11,9,11]
 
-a = int(input("Введіть 1 - додати вчителя/вивести вчителів. \nВведіть 2 - додати студента/вивести студентів:"))
-if a == 1:
-    aa = input("Що ви хочите? Вивести вчителів - 1, додати вчителів - 2.")
-    if aa == "1":
-        class Teachers(University):
-            tname = "Дмитро"
-            tage = 24
-            def __init__(self, tname, tage):
-                self.tname = tname
-                self.tage = tage
-                def print(self, tname, tage):
-                    print(self.tname, self.tage)
-dmitro = 
-
-univer = University()
-group = University()
-stund = Students()
-teacher = Teacher()
-subjects = Subjects()
-#print(human.age, human.job)
-print(group.name)
-print(subjects.subs)
-print(subjects.marks)
+# univer = University()
+# group = University()
+# stund = Students()
+# subjects = Subjects()
+# #print(human.age, human.job)
+# print(group.name)
+# print(subjects.subs)
+# print(subjects.marks)
